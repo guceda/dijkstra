5aba58a206d4c55520a22b20a77e972deed00c0c
Gonzalo Uceda Castro <gonzaloucedacastro@Gonzalos-MacBook-Pro.local>
2020-09-04 14:33:22 +0200

    Add method to create initial mesh

--- test/map.js
+++ test/map.js
@@ -1,30 +1,8 @@
 'use strict';
-
 const Graph = require('../lib/');
 
+let graph = new Graph();
+graph.generateMesh(10, 10, true);
 
-let map = new Graph();
-map.addNode('Fullstack');
-map.addNode('Starbucks');
-map.addNode('DigInn');
-map.addNode('Dubliner');
-map.addNode('Caffe');
-map.addNode('Insomnia');
+console.log(graph.findPath('0-0', '8-6'));
 
-
-map.addEdge('Fullstack', 'Starbucks', 6);
-map.addEdge('Fullstack', 'Dubliner', 2);
-map.addEdge('Fullstack', 'DigInn', 7);
-
-map.addEdge('DigInn', 'Dubliner', 4);
-map.addEdge('DigInn', 'Caffe', 9);
-
-map.addEdge('Caffe', 'Insomnia', 5);
-
-map.addEdge('Insomnia', 'Dubliner', 7);
-map.addEdge('Insomnia', 'Starbucks', 6);
-
-map.addEdge('Starbucks', 'Dubliner', 6);
-
-
-console.log(map.findPath('Fullstack', 'Caffe'));
