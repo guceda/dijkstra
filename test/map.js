fc194e4c33a9f46300d14961cc95cedfbe80b19a
Gonzalo Uceda Castro <gonzaloucedacastro@Gonzalos-MacBook-Pro.local>
2020-09-04 14:58:02 +0200

    separate mesh

--- test/map.js
+++ test/map.js
@@ -1,8 +1,9 @@
 'use strict';
 const Graph = require('../lib/');
+const generateMesh = require('../lib/mesh');
 
 let graph = new Graph();
-graph.generateMesh(10, 10, true);
+generateMesh(graph, 10, 10, true);
 graph.deleteNode('4-4');
 
 console.log(graph.findPath('0-0', '8-6'));
